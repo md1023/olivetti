c3dae76b384a55b54d21b9c86230b8aad8f5e57e
Maxim Nikolaev <simno87@gmail.com>
2014-03-04 11:36:01 +0400

    trials: dump and read functions prepared

--- trials/softline/core.py
+++ trials/softline/core.py
@@ -1,2 +1,29 @@
 # -*- coding: utf-8 -*-
+import urllib2
 from bs4 import BeautifulSoup
+
+DUMP_NAME = "dump.html"
+URL = "https://www.softlogic.ru/p/flexbby"
+
+
+def read_dump(filename):
+    page_dump = open(filename, "r")
+    page = page_dump.read()
+    page_dump.close()
+    return page
+
+
+def read_page(url):
+    response = urllib2.urlopen(url)
+    page = response.read()
+    return page
+
+
+def dump_page(page):
+    dump = open("./dump.html", "w")
+    dump.write(page.encode("utf-8"))
+    dump.close()
+
+
+if __name__ == "__main__":
+    pass
