02267ea467fcffea0c06969abb45b3af59028659
Maxim Nikolaev <simno87@gmail.com>
2014-03-04 19:42:55 +0400

    trials: string processed as html and escaped ones as xml

--- trials/softline/core.py
+++ trials/softline/core.py
@@ -35,15 +35,22 @@ def perform_change(attr, el):
 
 
 def traverse(page_source):
-    document = BeautifulSoup(page_source, "html5lib")
-    for el in list(document.find_all()):
+    document = BeautifulSoup(page_source, "html.parser")
+    for el in document.find_all()[1:]:
         if not "style" in str(el):
             continue
         perform_change("style", el)
+        if not "style" in str(el):
+            continue
+        # escaped strings parsed as xml
         xml = BeautifulSoup(el.text, "xml")
+        if xml.is_empty_element:
+            continue
         for sub_el in xml.find_all():
             perform_change("style", sub_el)
-
+        replacement = xml.find_all()[0]
+        el.string.replace_with(replacement)
+    return document
 
 if __name__ == "__main__":
     page_source = read_dump(DUMP_NAME)
