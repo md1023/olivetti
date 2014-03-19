0e8ef66ce92a9a8922acf10896b12fb4f4ffbce1
Maxim Nikolaev <simno87@gmail.com>
2014-03-19 16:58:48 +0400

    configs: don't ignore missing parameters, implented dry run

--- configs/create_links.sh
+++ configs/create_links.sh
@@ -3,19 +3,22 @@
 # и создаёт мягкие ссылки на эти файлы в директории откуда запущен.
 # Его следует запускать из папки пользователя по умолчанию
 # check if path to configuration files exists
-if [ ! -d $1 ]
-then
+if [ ! -d $1 ] || [[ $# -eq 0 ]] ; then
     echo -e "Specify path to configuration files like this:\n~/olivetti/configs/create_links.sh ~/olivetti/configs"
     exit 2
 fi
+
 echo -e "Links to files from:\n  $1\nWill be created in:\n  `pwd`\nCreating links:"
+
 # put link of each file from specified directory in current directory
 # this script is ignored along . and .. directories 
-for f in `ls -A --ignore='create_links.sh' $1`
-do
+for f in `ls -A --ignore='create_links.sh' $1`; do
     target="$1/$f"
     linkname="$f"
     echo "  $linkname -> $target"
+    if [ $# -eq 2 ] && [ $2 == "--dry" ] ; then
+	continue
+    fi
     ln -sfb $target $linkname
 done
 exit 0
