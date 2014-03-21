844b3e11fba7b91f3b021bdf0f33d37f8dec99ad
Maxim Nikolaev <simno87@gmail.com>
2014-03-21 12:02:47 +0400

    configs: root access and script directory location fixed

--- configs/.notion/ubuntu_session/setup_lightdm.sh
+++ configs/.notion/ubuntu_session/setup_lightdm.sh
@@ -3,16 +3,18 @@
 #check root access
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
-   # exit 1
+   exit 1
 fi
 
 # locate script location and destination folders
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 DEST="/usr/share"
 
-exit 1
 # perform copy
-cp ./xsessions.notion.desktop $DEST/xsessions/notion.desktop
-cp ./applications.notion.desktop $DEST/applications/notion.desktop
-cp ./gnome-session.notion.session $DEST/gnome-session/sessions/notion.session
-cp ./custom_notion_badge.png $DEST/unity-greeter/custom_notion_badge.png
+cp $DIR/xsessions.notion.desktop $DEST/xsessions/notion.desktop
+cp $DIR/applications.notion.desktop $DEST/applications/notion.desktop
+cp $DIR/gnome-session.notion.session $DEST/gnome-session/sessions/notion.session
+cp $DIR/custom_notion_badge.png $DEST/unity-greeter/custom_notion_badge.png
+
+echo "Files copied successfully"
+exit 0
