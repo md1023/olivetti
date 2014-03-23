61ecec5b54a8be5c20c154953ee9063be140c3ee
Maxim <maxim@vx3ubuntu.(none)>
2014-03-23 23:58:27 +0400

    configs: ln uses expanded paths, no arguments required

--- configs/create_links.sh
+++ configs/create_links.sh
@@ -3,22 +3,22 @@
 # и создаёт мягкие ссылки на эти файлы в директории откуда запущен.
 # Его следует запускать из папки пользователя по умолчанию
 # check if path to configuration files exists
-if [ ! -d $1 ] || [[ $# -eq 0 ]] ; then
-    echo -e "Specify path to configuration files like this:\n~/olivetti/configs/create_links.sh ~/olivetti/configs"
-    exit 2
-fi
+
+# locate script location and destination folders
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo -e "Links to files from:\n  $1\nWill be created in:\n  `pwd`\nCreating links:"
 
 # put link of each file from specified directory in current directory
 # this script is ignored along . and .. directories 
-for f in `ls -A --ignore='create_links.sh' $1`; do
-    target="$1/$f"
-    linkname="$f"
+for f in `ls -A --ignore='create_links.sh' $DIR`; do
+    target="$DIR/$f"
+    linkname="$HOME/$f"
     echo "  $linkname -> $target"
-    if [ $# -eq 2 ] && [ $2 == "--dry" ] ; then
+    if [ $# -eq 1 ] && [ $1 == "--dry" ] ; then
 	continue
     fi
     ln -sfb $target $linkname
+    # cp -psb $target $linkname
 done
 exit 0
