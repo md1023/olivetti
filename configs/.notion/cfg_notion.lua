6de6db39537c0b32bdc94684d4838c7dedb60ab8
Maxim <maxim@vx3ubuntu.(none)>
2014-03-23 23:42:11 +0400

    config, notion: using adereth's setup with alterations

--- configs/.notion/cfg_notion.lua
+++ configs/.notion/cfg_notion.lua
@@ -1,109 +1,50 @@
 --
--- Ion main configuration file
+-- Notion main configuration file
 --
 -- This file only includes some settings that are rather frequently altered.
--- The rest of the settings are in cfg_ioncore.lua and individual modules'
+-- The rest of the settings are in cfg_notioncore.lua and individual modules'
 -- configuration files (cfg_modulename.lua). 
---
--- When any binding and other customisations that you want are minor, it is 
--- recommended that you include them in a copy of this file in ~/.ion3/.
--- Simply create or copy the relevant settings at the end of this file (from
--- the other files), recalling that a key can be unbound by passing 'nil' 
--- (without the quotes) as the callback. For more information, please see 
--- the Ion configuration manual available from the Ion Web page.
---
 
--- Set default modifiers. Alt should usually be mapped to Mod1 on
--- XFree86-based systems. The flying window keys are probably Mod3
--- or Mod4; see the output of 'xmodmap'.
 META="Mod4+"
 --ALTMETA=""
 
 -- Terminal emulator
---XTERM="gnome-terminal"
-XTERM="terminator -p zen"
+if os and os.execute("test -x /usr/bin/terminator") == 0 then
+    XTERM="/usr/bin/terminator"
+else
+    XTERM="/usr/bin/xterm"
+end
+
+-- Program launcher
+if os and os.execute("test -x /usr/bin/gmrun") == 0 then
+    LAUNCHER="ioncore.exec_on(_, '/usr/bin/gmrun')"
+else
+    LAUNCHER="mod_query.query_exec(_)"
+end
 
 -- Some basic settings
 ioncore.set{
     -- Maximum delay between clicks in milliseconds to be considered a
     -- double click.
-    --dblclick_delay=250,
+    dblclick_delay=250,
 
-    -- For keyboard resize, time (in milliseconds) to wait after latest
-    -- key press before automatically leaving resize mode (and doing
-    -- the resize in case of non-opaque move).
-    --kbresize_delay=1500,
+    opaque_resize=true,
 
-    -- Opaque resize?
-    --opaque_resize=false,
+    -- Don't move the mouse cursor when changing frames with the keyboard
+    warp=false,
 
-    -- Movement commands warp the pointer to frames instead of just
-    -- changing focus. Enabled by default.
-    -- warp=false,
-    
-    -- Switch frames to display newly mapped windows
-    --switchto=true,
-    
     -- Default index for windows in frames: one of 'last', 'next' (for
     -- after current), or 'next-act' (for after current and anything with
     -- activity right after it).
-    --frame_default_index='next',
+    frame_default_index='next-act',
     
     -- Auto-unsqueeze transients/menus/queries.
-    --unsqueeze=true,
+    unsqueeze=true,
     
     -- Display notification tooltips for activity on hidden workspace.
+    -- This is awesome in conjunction with urgentOnBell and a bell in the PROMPT
     screen_notify=true,
 }
 
-
--- Load default settings. The file cfg_defaults loads all the files
--- commented out below, except mod_dock. If you do not want to load
--- something, comment out this line, and uncomment the lines corresponding
--- the the modules or configuration files that you want, below.
--- The modules' configuration files correspond to the names of the 
--- modules with 'mod' replaced by 'cfg'.
 dopath("cfg_defaults")
-
--- Load configuration of the Ion 'core'. Most bindings are here.
---dopath("cfg_ioncore")
-
--- Load some kludges to make apps behave better.
--- dopath("cfg_kludges")
-
--- Define some layouts. 
---dopath("cfg_layouts")
-
--- Load some modules. Bindings and other configuration specific to modules
--- are in the files cfg_modulename.lua.
---dopath("mod_query")
---dopath("mod_menu")
---dopath("mod_tiling")
---dopath("mod_statusbar")
---dopath("mod_dock")
---dopath("mod_sp")
-dopath("statusd_volume")
-
---
--- Common customisations
---
-
-dopath("simno_config")
-
--- Uncommenting the following lines should get you plain-old-menus instead
--- of query-menus.
-
---defbindings("WScreen", {
---    kpress(ALTMETA.."F12", "mod_menu.menu(_, _sub, 'mainmenu', {big=true})"),
---})
---
---defbindings("WMPlex.toplevel", {
---    kpress(META.."M", "mod_menu.menu(_, _sub, 'ctxmenu')"),
---})
-
-defwinprop {
-           class = "Synapse",
-           float = true,
-           transient_mode = "off",
-           userpos = true,
-}
+dopath("look_simno")
