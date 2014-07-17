3d66546ceeb57b2182cc4c901679a3dc3f5ff940
Maxim Nikolaev <simno87@gmail.com>
2014-07-17 17:27:31 +0400

    algo2: elements' class introduced

--- trials/unionfind.py
+++ trials/unionfind.py
@@ -1,16 +1,25 @@
+class Element(object):
+    def __init__(self, element_id, leader=None):
+        self.id = element_id
+        if not leader:
+            leader = self
+        self.leader = leader
+        self.rank = 0
+
+    def __repr__(self):
+        d = "%s" % self.id
+        return d
+
+
 class DisjointSet(object):
     """
     >>> names = "Thaddeus Max Larabee Siegfried Hans Schwartz".split()
     >>> s = DisjointSet(names)
-    >>> s.leaders == [0, 1, 2, 3, 4, 5]
-    True
-    >>> s.union("Max", "Thaddeus")
-    >>> s.leaders == [0, 0, 2, 3, 4, 5]
-    True
+    >>> s.union(s["Max"], s["Thaddeus"])
     """
 
     def __init__(self, elements):
-        self.__elements = elements
+        self.__elements = [Element(e) for e in elements]
         self.leaders = range(len(self.__elements))
 
     def indexer(self, person):
@@ -19,40 +28,42 @@ class DisjointSet(object):
         leader_name = self.__elements[leader_index]
         return (index, leader_index, leader_name)
 
-    def __getitem__(self, person):
-        """
-        >>> names = "Thaddeus Max Larabee Siegfried Hans Schwartz".split()
-        >>> s = DisjointSet(names)
-        >>> s.union("Max", "Thaddeus")
-        >>> s["Max"] == "Thaddeus"
-        True
-        >>> s.union("Thaddeus", "Siegfried")
-        >>> (s["Max"], s["Thaddeus"]) == ("Siegfried", "Siegfried")
-        True
-        """
-        index, leader_index, leader_name = self.indexer(person)
-
-        if index != leader_index:
-            return self[leader_name]
-        return leader_name
+    def __getitem__(self, element_id):
+        for element in self.__elements:
+            if element.id == element_id:
+                return element
+        return None
 
     def find(self, person):
-        index, leader_index, leader_name = self.indexer(person)
+        # index, leader_index, leader_name = self.indexer(person)
 
-        if index != leader_index:
-            return self.find(leader_name)
-        return leader_index
+        # if index != leader_index:
+        #     return self.find(leader_name)
+        leader = person.leader
+        if person != leader:
+            # find leader in hierarchy
+            self.find(person.leader)
+
+        # return leader_index
+        return person
 
     def union(self, person1, person2):
         """
         >>> names = "Thaddeus Max Larabee Siegfried Hans Schwartz".split()
         >>> s = DisjointSet(names)
-        >>> s["Max"].leader == "Max"
+        >>> s["Max"].leader == s["Max"]
+        True
+        >>> s.union(s["Max"], s["Thaddeus"])
+        >>> s["Max"].leader == s["Thaddeus"]
+        True
+        >>> s.union(s["Thaddeus"], s["Siegfried"])
+        >>> (s.find("Max"), s.find("Thaddeus")), (s["Siegfried"], s["Siegfried"])
+        True
         """
         x = self.find(person1)
         y = self.find(person2)
-        self.leaders[x] = y
-        # x.leader = y
+        # self.leaders[x] = y
+        x.leader = y
 
         return
         if x == y:
