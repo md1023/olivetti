fb2ca0adb83527cd283d785ba1c39ec642601ddf
Maxim Nikolaev <simno87@gmail.com>
2014-10-08 19:48:39 +0000

    ergosolo: test three finished

--- trials/ergosolo/test3.py
+++ trials/ergosolo/test3.py
@@ -2,15 +2,43 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
+from os.path import isfile
 
-users = [(u"Платон Щукин",), (u"Лера Страза",), (u"Георгий Атласов",)]
-message = [(1, u"Привет, Платон"),
+DB_NAME = "chat.db"
+USERS = [(u"Платон Щукин",), (u"Лера Страза",), (u"Георгий Атласов",)]
+MESSAGES = [(1, u"Привет, Платон"),
            (3, u"Срочно пришли карту."),
            (3, u"Жду на углу Невского и Тверской."),
            (1, u"Это снова я, пиши чаще")]
 
-db = sqlite3.connect('chat.db')
+if not isfile(DB_NAME):
+    raise Exception("\nСоздайте таблицы командой:\nsqlite3 chat.db < test3.sql")
+
+db = sqlite3.connect(DB_NAME)
 cursor = db.cursor()
-cursor.executemany("insert into users (name) values (?)", users)
+cursor.executemany("insert into users (name) values (?)", USERS)
+cursor.executemany("insert into messages (uid, msg) values (?, ?)", MESSAGES)
 db.commit()
+
+cursor.execute("""
+   select u.name, count(m.id) from users u
+   join messages m on m.uid = u.id
+   group by u.id;
+   """)
+
+print "Количество сообщений от пользователей:"
+for user, count in cursor:
+    print user, count
+
+cursor.execute("""
+   select u.name, count(m.id) from users u
+   left join messages m on m.uid = u.id
+   group by u.id
+   having count(m.id) = 0;
+   """)
+
+print "Пользователи не отправлявшие сообщений:"
+for user, count in cursor:
+    print user, count
+
 db.close()
--- trials/ergosolo/test3.sql
+++ trials/ergosolo/test3.sql
@@ -1,10 +1,10 @@
 create table if not exists users (
-  uid integer primary key autoincrement not null,
+  id integer primary key autoincrement not null,
   name varchar(30)
 );
 
 create table if not exists messages (
-  uid integer primary key autoincrement not null,
-  id integer not null,
+  id integer primary key autoincrement not null,
+  uid integer not null,
   msg varchar(30)
 );
