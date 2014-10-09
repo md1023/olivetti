972d66448c1c407e0ebc7394af755f42b82d0b52
Maxim Nikolaev <simno87@gmail.com>
2014-10-09 16:31:01 +0000

    ergosolo: state transition restored

--- trials/ergosolo/test2.py
+++ trials/ergosolo/test2.py
@@ -7,7 +7,6 @@ class State(object):
     def transition_handler(self, input, regexp, state):
         # print self, state, input,regexp
         if re.match(regexp, input, flags=re.IGNORECASE):
-            print "PASS", state
             return state
 
 class S0(State):
@@ -16,7 +15,8 @@ class S0(State):
 
 class S1(State):
     def run(self, input):
-        return self.transition_handler(input, "[A-Z0-9]", "S1")
+        return self.transition_handler(input, "[A-Z0-9]", "S1") or \
+            self.transition_handler(input, "[.-]", "S2")
 
 class S2(State):
     def run(self, input):
@@ -34,10 +34,8 @@ class Machine(object):
         self.current_state = state
 
     def run(self, input):
-        state = self.states[self.current_state].run(input)
-        print self.current_state, state
-        if state:
-            return state
+        self.current_state = self.states[self.current_state].run(input)
+        return self.current_state
 
 class LoginChecker(object):
     def __init__(self, login_length = None):
@@ -55,17 +53,19 @@ class LoginChecker(object):
         return self.machine.current_state
 
 if __name__ == "__main__":
-    r = "^[A-Z][A-Z0-9.-]{0,18}?[A-Z0-9]?$"
+    pattern = re.compile(
+        "^[A-Z]([A-Z0-9.-]{0,18}?[A-Z0-9])?$",
+        flags=re.IGNORECASE)
+
     s1 = "  a" + "1" * 18 + "z"
     s2 = "a" + "0" * 17 + "z  "
     s3 = "a" + "-" * 18 + "z"
-    s4 = "z"
+    s4 = "z0"
+    s5 = "a1.ba1.b"
 
     l = LoginChecker()
 
-    for s in [s1, s2, s3, s4]:
-        print s
-        m = re.match(r, s.strip(), flags=re.IGNORECASE)
+    for s in [s1, s2, s3, s4, s5]:
+        m = pattern.match(s.strip())
         assert m.group(0)
-        last_state = l.run(s)
-        assert last_state == "S1", last_state
+        assert l.run(s) == "S1"
