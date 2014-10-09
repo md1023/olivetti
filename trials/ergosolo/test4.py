ebfc439bf107e3c3b3e2ea6438f5414b36e4aedd
Maxim Nikolaev <simno87@gmail.com>
2014-10-09 19:37:01 +0000

    ergosolo: server log report finished

--- trials/ergosolo/test4.py
+++ trials/ergosolo/test4.py
@@ -1,1 +1,20 @@
-# cut -d ' ' -f 1 server.log | uniq -c | sort | tail -5
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import operator
+
+# Поиск в консоли:
+# cut -d ' ' -f 1 server.log | sort | uniq -c | sort | tail -5
+
+ips = dict()
+
+with open("server.log", "r") as f:
+    for line in f:
+        ip = line.partition(" ")[0]
+        if ip not in ips:
+            ips[ip] = 0
+        ips[ip] += 1
+    top = sorted(ips.items(), key=operator.itemgetter(1))[-5:]
+
+for ip, count in top:
+    print count, ip
