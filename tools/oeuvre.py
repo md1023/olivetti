7541404edfe280dc639e0b11ab75f31e859c8b74
Maxim Nikolaev <simno87@gmail.com>
2015-06-16 12:00:04 +0300

    tools: late commit for oeuvre project

--- tools/oeuvre.py
+++ tools/oeuvre.py
@@ -11,20 +11,25 @@ def get_mp3s(folder="."):
             [os.path.join(path, name) for name in files if name[-4:] == ".mp3"])
     return mp3s
 
-def get_song_info(mp3s, predicate=lambda:None, chain=lambda:None, fields=("artist", "title")):
+def get_song_info(mp3s, gen,
+                  # predicate=lambda:None, chain=lambda:None,
+                  fields=("artist", "title")):
     for name in mp3s:
         mp3 = eyed3.load(name)
         song = []
         for f in (getattr(mp3.tag, f, None) for f in fields):
-            if predicate(f):
-                song += chain(f),
+            zzz = gen(f)
+            print ">", zzz
+            song += zzz
+            # if predicate(f):
+            #     song += chain(f),
         yield name, song
 
 if __name__ == "__main__":
     mp3s = get_mp3s()
     for s in get_song_info(
-            mp3s,
-            predicate = lambda s: s, # "," not in (s or ""),
-            chain = lambda s: s): # s.split(",")):
+            mp3s, lambda s: tuple(s.split(",") if s else s)):
+            # predicate = lambda s: s, # "," not in (s or ""),
+            # chain = lambda s: s): # s.split(",")):
         # continue
         print s
