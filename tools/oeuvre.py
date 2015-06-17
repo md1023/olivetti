b7f9609c1d6905f1d1eb634bfb0de70fffc152ae
Maxim Nikolaev <simno87@gmail.com>
2015-06-17 23:27:55 +0300

    tools: oeuvre split time strings

--- tools/oeuvre.py
+++ tools/oeuvre.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
 import os
 import eyed3
+
+CONSTS=dict(time="[0-9]+:[0-9]")
 
 def get_mp3s(folder="."):
     mp3s = []
@@ -11,25 +14,25 @@ def get_mp3s(folder="."):
             [os.path.join(path, name) for name in files if name[-4:] == ".mp3"])
     return mp3s
 
-def get_song_info(mp3s, gen,
-                  # predicate=lambda:None, chain=lambda:None,
+def get_song_info(mp3s, # gen,
+                  predicate=lambda:None,
+                  chain=lambda:None,
                   fields=("artist", "title")):
     for name in mp3s:
         mp3 = eyed3.load(name)
-        song = []
+        info = (name,)
         for f in (getattr(mp3.tag, f, None) for f in fields):
-            zzz = gen(f)
-            print ">", zzz
-            song += zzz
-            # if predicate(f):
-            #     song += chain(f),
-        yield name, song
+            info += (f,)
+        yield info
+
+def split_song_name(info):
+    artist, name = info[1:]
+    if artist and name:  # and re.search(): check if time is in name!
+        comma = lambda s: s.split(",") if "," in s else s
+        print comma(artist), "-", comma(name), \
+            re.search("(%(time)s+)(?:-)?(%(time)s+)? (.*)" % CONSTS, name).groups()
 
 if __name__ == "__main__":
     mp3s = get_mp3s()
-    for s in get_song_info(
-            mp3s, lambda s: tuple(s.split(",") if s else s)):
-            # predicate = lambda s: s, # "," not in (s or ""),
-            # chain = lambda s: s): # s.split(",")):
-        # continue
-        print s
+    for s in get_song_info(mp3s):
+        split_song_name(s)
