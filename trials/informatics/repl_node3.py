325f36b439e2aaa6ec64a958b782758531006df6
Maxim Nikolaev <simno87@gmail.com>
2016-12-29 12:41:35 +0300

    informatics: real and int detected

--- trials/informatics/repl_node3.py
+++ trials/informatics/repl_node3.py
@@ -2,16 +2,14 @@
 import re
 
 
-class Node:
-    pass
-
-
-class Terminal(Node):
-    def __init__(self, value):
-        self.value = value
-
+class Printable:
     def __repr__(self):
         return "{0}[{1}]".format(self.__class__.__name__, self.value)
+
+
+class Terminal(Printable):
+    def __init__(self, value):
+        self.value = value
 
     
 class Digit(Terminal):
@@ -29,6 +27,11 @@ class End(Terminal):
 class TokenNotFound(Exception):
     pass
 
+
+class RuleNotFound(Exception):
+    pass
+
+
 class Lexer:
     map = [Digit, Dot, End]
     
@@ -44,5 +47,35 @@ class Lexer:
                 raise TokenNotFound('unknown character \'{0}\''.format(c))
             yield token
 
-l = Lexer('1.2$')
-print(list(l.tokens))
+
+class Node(Printable):
+    def __init__(self, value, subnodes=None):
+        if subnodes:
+            self.subnodes = subnodes
+        self.value = value
+
+
+class Integer(Node):
+    regex = '^\d+$'
+
+
+class Real(Node):
+    regex = '^\d+\.(\d+)?$|^(\d+)?\.\d+$'
+
+
+class Parser:
+    hierarchy = [Real, Integer]
+
+    def __init__(self, input):
+        pass
+        self.input = input
+
+    def get_rule(self, input):
+        for N in self.hierarchy:
+            if self.match_rule(N, input):
+                return N(input)
+        raise RuleNotFound(input)
+
+
+    def match_rule(self, node, text):
+        return re.match(node.regex, text)
--- trials/informatics/test_repl_node2.py
+++ trials/informatics/test_repl_node2.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 # test_repl_node2.py
 
+import re
 from unittest import TestCase, main
 from repl_node2 import Parser, ParserError
-from repl_node3 import Lexer, TokenNotFound, Digit, Dot, End
+from repl_node3 import Lexer, \
+    TokenNotFound, RuleNotFound, \
+    Digit, Dot, End, \
+    Integer, Real, \
+    Parser as RegexParser
+
 
 class ParserTestCase(TestCase):
     def setUp(self):
@@ -67,6 +73,52 @@ class LexerTestCase(TestCase):
             str(list(self.l('1.2$').tokens)),
             str([Digit('1'), Dot('.'), Digit('2'), End('$')])
         )
+
+
+class RealTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_right(self):
+        self.assertTrue(re.match(Real.regex, '1.2'))
+        self.assertTrue(re.match(Real.regex, '1.23'))
+        self.assertTrue(re.match(Real.regex, '13.2'))
+        self.assertTrue(re.match(Real.regex, '.2'))
+        self.assertTrue(re.match(Real.regex, '1.'))
+
+    def test_fail_integer(self):
+        self.assertFalse(re.match(Real.regex, '1'))
+        self.assertFalse(re.match(Real.regex, '12'))
+
+    def test_wrong(self):
+        self.assertFalse(re.match(Real.regex, '.12.'))
+        self.assertFalse(re.match(Real.regex, '1..2'))
+        self.assertFalse(re.match(Real.regex, '1..'))
+        self.assertFalse(re.match(Real.regex, '..2'))
+        self.assertFalse(re.match(Real.regex, '..'))
+        self.assertFalse(re.match(Real.regex, '.'))
+        self.assertFalse(re.match(Real.regex, ''))
+
+    def test_inadequate(self):
+        self.assertFalse(re.match(Real.regex, 'a'))
+        self.assertFalse(re.match(Real.regex, 'a.'))
+        self.assertFalse(re.match(Real.regex, '.a'))
+
+
+class RegexParserTestCase(TestCase):
+    def setUp(self):
+        self.p = RegexParser('')
+
+    def test_1(self):
+        input = '123'
+        self.assertTrue(isinstance(self.p.get_rule(input), Integer))
+
+        input = '1.2'
+        self.assertTrue(isinstance(self.p.get_rule(input), Real))
+
+        input = 'a'
+        self.assertRaises(RuleNotFound, self.p.get_rule, input)
+
 
 if __name__ == '__main__':
     main()
