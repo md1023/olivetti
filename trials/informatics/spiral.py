3cedd7f13dcb116944d03553aae75e1489a5a803
Maxim Nikolaev <simno87@gmail.com>
2016-12-31 00:43:21 +0300

    informatics: spiral headings added

--- trials/informatics/spiral.py
+++ trials/informatics/spiral.py
@@ -1,29 +1,16 @@
+#!/usr/bin/env python3
 import sys
 from collections import namedtuple
+from itertools import cycle
 
-heading = dict(
-    GO_UP=('y', 1),
-    GO_DOWN=('y', -1),
-    GO_LEFT=('x', -1),
-    GO_RIGHT=('x', 1)
+DIRECTIONS = dict(
+    # x y 
+    GO_UP=(0, -1),
+    GO_RIGHT=(1, 0),
+    GO_DOWN=(0, 1),
+    GO_LEFT=(-1, 0)
 )
-
-# heading = {
-#     ('y', 1) = 'GO_UP'
-#     ('y', -1) = 'GO_DOWN'
-#     ('x', -1) = 'GO_LEFT'
-#     ('x', 1) = 'GO_RIGHT'
-# }
-
-def heading(index):
-    if bool(index % 2) and not int(index / 2):
-        return 'GO_UP'
-    if bool(index % 2) and int(index / 2):
-        return 'GO_DOWN'
-    if not bool(index % 2) and int(index / 2):
-        return 'GO_LEFT'
-    if not bool(index % 2) and not int(index / 2):
-        return 'GO_RIGHT'
+DIRECTION_NAMES = ('GO_RIGHT', 'GO_DOWN', 'GO_LEFT', 'GO_UP')
 
 def spiralize(size):
 
@@ -32,28 +19,47 @@ def spiralize(size):
 
     # lengths of steps
     l = lambda size: [i for i in range(size, 0, -2)];
-
     p = lambda size: [size - 1] + [j-1 for i in l(size) for j in [i,i]][:size-1]
-    print(p(size))
 
     Point = namedtuple('Point', ['x', 'y'])
     points = [Point(0, 0)]
 
-    for index, step in enumerate(p(size)):
-        # heading = None
-        coordinate = 'y' if bool(index % 2) else 'x'
-        direction = -1 if int(index / 2) % 2 else 1
+    DIRECTION = cycle(DIRECTION_NAMES)
+    heading = next(DIRECTION)
 
+    for index, step in enumerate(p(size)):
+        dx, dy = DIRECTIONS.get(heading)    
         for i in range(step):
-            # newPoint = points[-1]._replace(
-            #    **{coordinate: getattr(points[-1], coordinate) + direction})
-            last_point = points[-1]
-            newPoint = last_point._replace(
-                **{coordinate: getattr(last_point, coordinate) + direction})
-            points.append(newPoint)
+            x, y = points[-1]
+            p = Point(x + dx, y + dy)
+            points.append(p)
+        heading = next(DIRECTION)
 
     m = [[0 for i in range(size)] for j in range(size)]
     for p in points:
         m[p.y][p.x] = 1
 
     return m
+
+assert(spiralize(1) == [[1]])
+assert(spiralize(2) == [[1,1],
+                                  [0,1]])
+assert(spiralize(3) == [[1,1,1],
+                                  [0,0,1],
+                                  [1,1,1]])
+assert(spiralize(5) == [[1,1,1,1,1],
+                                  [0,0,0,0,1],
+                                  [1,1,1,0,1],
+                                  [1,0,0,0,1],
+                                  [1,1,1,1,1]])
+assert(spiralize(8) == [[1,1,1,1,1,1,1,1],
+                                  [0,0,0,0,0,0,0,1],
+                                  [1,1,1,1,1,1,0,1],
+                                  [1,0,0,0,0,1,0,1],
+                                  [1,0,1,0,0,1,0,1],
+                                  [1,0,1,1,1,1,0,1],
+                                  [1,0,0,0,0,0,0,1],
+                                  [1,1,1,1,1,1,1,1]])
+
+
+
