683382351f441619512f9c42aa135195c952932b
Maxim Nikolaev <simno87@gmail.com>
2017-01-02 02:42:09 +0300

    informatics: started to yield snake coordinates

--- trials/informatics/spiral.py
+++ trials/informatics/spiral.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import sys
-from collections import namedtuple
 from itertools import cycle
 
 
@@ -23,7 +21,7 @@ DIRECTIONS = dict(
     # .
     GO_UP=(0, -1, 1, 0)
 )
-DIRECTION_ORDER = ('GO_RIGHT', 'GO_DOWN', 'GO_LEFT', 'GO_UP')
+
 
 def spiralize(size):
 
@@ -33,29 +31,35 @@ def spiralize(size):
     if size == 1:
         return [[1]]
 
-    # lengths of steps
+    m = [[0 for i in range(size)] for j in range(size)]
+    m[0][0] = 1
+    m[1][0] = 0
+
+    for x, y, value in move(size):
+        m[y][x] = value
+
+    return m
+
+
+def move(size):
+    # lengths of straight moves
     l = lambda size: [i for i in range(size, 0, -2)];
 
-    # path parts
+    # snake's path in steps
     p = lambda size: [size - 1] + [j-1 for i in l(size) for j in [i,i]][:size-1]
 
-    Point = namedtuple('Point', ['x', 'y', 'value'])
-    points = [Point(0, 0, '1'), Point(0, 1, '0')]
+    direction = cycle(('GO_RIGHT', 'GO_DOWN', 'GO_LEFT', 'GO_UP'))
 
-    DIRECTION = cycle(DIRECTION_ORDER)
-    heading = next(DIRECTION)
+    x, y = 0, 0
 
-    for index, step in enumerate(p(size)):
-        dx, dy, nx, ny = DIRECTIONS.get(heading)
-        for i in range(step):
-            x, y, value = points[-2]
-            p1 = Point(x + dx, y + dy, '1')
-            points.append(p1)
-            p2 = Point(x + dx + nx, y + dy + ny, '0')
-            points.append(p2)
-        heading = next(DIRECTION)
+    for steps in p(size):
+        # turn right (move clockwise)
+        dx, dy, nx, ny = DIRECTIONS.get(next(direction))
 
-    m = [['.' for i in range(size)] for j in range(size)]
-    for count, p in enumerate(points):
-        m[p.y][p.x] = int(p.value)
-    return m
+        # go straight (zero is snake's shadow)
+        for _ in range(steps):
+            x += dx
+            y += dy
+            yield x, y, 1
+            yield x + nx, y + ny, 0
+
