c37e89783b81416ab21ab4d95bb29e103d5eb15d
Maxim Nikolaev <simno87@gmail.com>
2017-01-05 00:27:45 +0300

    informatics: snake crawls the spiral path

--- trials/informatics/spiral.py
+++ trials/informatics/spiral.py
@@ -23,6 +23,49 @@ DIRECTIONS = dict(
 )
 
 
+class Snake:
+    def __init__(self, size):
+        self.size = size
+        self.x, self.y = 0, 0
+        self.path = SpiralPath(size)
+
+    def step(self):
+        body_x, body_y, shadow_x, shadow_y = DIRECTIONS.get(self.direction)
+        self.x += body_x
+        self.y += body_y
+        yield self.x, self.y, 1
+        yield self.x + shadow_x, self.y + shadow_y, 0
+
+    def move(self):
+        direction = cycle(('GO_RIGHT', 'GO_DOWN', 'GO_LEFT', 'GO_UP'))
+
+        for steps in self.path.move():
+            self.direction = next(direction)
+            for _ in range(steps):
+                yield from self.step()
+
+
+class Path(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.path = iter(self._path)
+
+    def move(self):
+        for step in self.path:
+            yield step
+
+
+class SpiralPath(Path):
+    def __init__(self, size):
+        # lengths of straight moves
+        l = lambda size: [i for i in range(size, 0, -2)];
+        # spiral path in steps
+        self._path = [size - 1] + [j-1 for i in l(size) for j in [i,i]][:size-1]
+        super(SpiralPath, self).__init__()
+
+
 def spiralize(size):
 
     if not size:
@@ -35,31 +78,10 @@ def spiralize(size):
     m[0][0] = 1
     m[1][0] = 0
 
-    for x, y, value in move(size):
+    snake = Snake(size)
+    for x, y, value in snake.move():
         m[y][x] = value
 
     return m
 
-
-def move(size):
-    # lengths of straight moves
-    l = lambda size: [i for i in range(size, 0, -2)];
-
-    # snake's path in steps
-    p = lambda size: [size - 1] + [j-1 for i in l(size) for j in [i,i]][:size-1]
-
-    direction = cycle(('GO_RIGHT', 'GO_DOWN', 'GO_LEFT', 'GO_UP'))
-
-    x, y = 0, 0
-
-    for steps in p(size):
-        # turn right (move clockwise)
-        dx, dy, nx, ny = DIRECTIONS.get(next(direction))
-
-        # go straight (zero is snake's shadow)
-        for _ in range(steps):
-            x += dx
-            y += dy
-            yield x, y, 1
-            yield x + nx, y + ny, 0
 
