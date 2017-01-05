007ba6f3ed6ace90753a29d480038194588c7bd0
Maxim Nikolaev <simno87@gmail.com>
2017-01-05 23:25:47 +0300

    informatics: object oriented approach

--- trials/informatics/spiral.py
+++ trials/informatics/spiral.py
@@ -3,53 +3,31 @@ from itertools import cycle
 
 
 class Snake:
-    def __init__(self, dimension):
-        self.size = dimension.size
-        self.path = SpiralPath(self.size)
+    def __init__(self, dimension, PathType):
         self.dimension = dimension
-        self.direction = self.path.directions[0]
-
-    def step(self, qqq):
-        yield from self.path.step(self.dimension, qqq)
+        self.path = PathType(dimension.size)
+        self.body = 1
+        self.shadow = 0
 
     def move(self):
         direction = cycle(self.path.directions)
 
-        for steps in self.path.move():
-            qqq = next(direction)
+        for steps in self.path.traverse():
+            self.direction = next(direction)
             for _ in range(steps):
-                yield from self.step(qqq)
+                yield from self.dimension.step(self)
 
 
-class Path(object):
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.x = 0
-        self.y = 0
-        self.path = iter(self._path)
-
-    def move(self):
-        for step in self.path:
-            yield step
-
-
-class SpiralPath(Path):
+class SpiralPath:
     directions = ('GO_RIGHT', 'GO_DOWN', 'GO_LEFT', 'GO_UP')
     def __init__(self, size):
         # lengths of straight moves
         l = lambda size: [i for i in range(size, 0, -2)];
         # spiral path in steps
         self._path = [size - 1] + [j-1 for i in l(size) for j in [i,i]][:size-1]
-        super(SpiralPath, self).__init__()
 
-    def step(self, dimension, qqq):
-        self.x, self.y, shadow_x, shadow_y = dimension.directions.get(qqq)
-        # body
-        yield self.x, self.y, 1
-        # shadow
-        yield self.x + shadow_x, self.y + shadow_y, 0
+    def traverse(self):
+        yield from iter(self._path)
 
 
 class Board(object):
@@ -76,7 +54,14 @@ class Board(object):
     def __init__(self, size):
         self.size = size
 
-    def draw(self, figure_name):
+    def step(self, figure):
+        new_x, new_y, shadow_x, shadow_y = self.directions.get(figure.direction)
+        figure.x += new_x
+        figure.y += new_y
+        yield figure.x, figure.y, figure.body
+        yield figure.x + shadow_x, figure.y + shadow_y, figure.shadow
+
+    def draw(self, Figure, PathType):
 
         if not self.size:
             return []
@@ -84,20 +69,18 @@ class Board(object):
         if self.size == 1:
             return [[1]]
 
-        m = [[0 for i in range(self.size)] for j in range(self.size)]
-        m[0][0] = 1
-        m[1][0] = 0
+        figure = Figure(self, PathType)
+        figure.x, figure.y = 0, 0
 
-        figure = figure_name(self)
-        for x, y, value in figure.move():
-            m[y][x] = value
+        m = [[figure.shadow for i in range(self.size)] for j in range(self.size)]
+        m[0][0] = figure.body
+        m[1][0] = figure.shadow
+
+        for x, y, sign in figure.move():
+            m[y][x] = sign
 
         return m
 
 
 def spiralize(size):
-    board = Board(size)
-    m = board.draw(Snake)
-    print(m)
-
-
+    return Board(size).draw(Snake, SpiralPath)
