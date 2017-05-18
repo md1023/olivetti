3764660ddbeb0da31e5522d91a152d1ed33498f8
Maxim Nikolaev <simno87@gmail.com>
2017-05-18 19:00:15 +0300

    codewars: finished decoding ascii85

--- trials/pss.py
+++ trials/pss.py
@@ -2,11 +2,7 @@ import re
 import math
 
 
-text = '''Man is distinguished, not only by his reason, but by this
-singular passion from other animals, which is a lust of the mind, that
-by a perseverance of delight in the continued and indefatigable
-generation of knowledge, exceeds the short vehemence of any carnal
-pleasure.'''
+text = '''Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.'''
 # text = 'easy'
 # text = 'somewhat difficult'
 
@@ -37,22 +33,43 @@ def ascii_to_ascii85_gen(text):
         yield from chars85
 
 
-ascii85 = ''.join(ascii_to_ascii85_gen(text))
-print(ascii85)
+def ascii85_to_ascii_gen(text):
+    for chunk in re.finditer('.{1,5}', text):
+        # convert chars into ascii codes and pad u's to chunk
+        # lesser than 5 characters
+        ordinals = (
+            ord(c) - 33 for c in chunk.group().ljust(5, 'u')
+        )
+        # turn combination of five ascii codes into 32-bit integer
+        base2 = sum(
+            code * 85 ** (i - 1)
+            for i, code in zip(range(5, 0, -1), ordinals)
+        )
+        # turn base2 into 32-bit form
+        binarybase2 = bin(base2)[2:].zfill(32)
+        # break 32-bit form into groups of four bytes
+        codes = (
+            int(b, 2) for b in re.findall('.{8}', binarybase2)
+        )
+        # get characters from ascii codes and throw away as much
+        # characters as much u's were padded to a chunk
+        chars = list(chr(c) for c in codes)[
+            :
+            4 - (5 - len(chunk.group()))
+        ]
+        yield from chars
 
-for chunk in list(re.finditer('.{1,5}', ascii85))[-2:]:
-    # convert chars into ascii codes and pad zero bytes to chunk
-    # lesser than 5 characters
-    # for c in chunk.group().ljust(5, '\0'):
-    #     print(c)
-    ordinals = list(
-        ord(c) - 33 for c in chunk.group().ljust(5, 'u')
-    )
-    # turn combination of five ascii codes into 32-bit integer
-    base2 = sum(
-        code * 85 ** (i - 1)
-        for i, code in zip(range(5, 0, -1), ordinals)
-    )
-    binarybase2 = bin(base2)[2:].zfill(32)
 
-    print(chunk.group(), ordinals, base2, grouped_bits)
+def ascii_to_ascii85(text):
+    return ''.join(ascii_to_ascii85_gen(text))
+
+
+def ascii85_to_ascii(text):
+    return ''.join(ascii85_to_ascii_gen(text))
+
+
+print(text)
+print(ascii_to_ascii85(text))
+print(ascii85_to_ascii(ascii_to_ascii85(text)))
+
+assert text == ascii85_to_ascii(ascii_to_ascii85(text))
